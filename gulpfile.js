e086775f419689340966ef62b411b7c3589ec0d1
Sherwin Techico <stechico@gmail.com>
2014-12-22 03:39:07 +0800

    Use BrowserSync, instead of Livereload.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,32 @@
 // Include gulp
-var gulp = require('gulp'); 
- 
+var gulp = require('gulp');
+
 // Include Our Plugins
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var inlineCss = require('gulp-inline-css');
-var livereload = require('gulp-livereload');
-var lr = require('tiny-lr');
-var server = lr();
- 
- 
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+
 // Compile Our Sass
 gulp.task('sass', function() {
     return gulp.src('src/scss/*.scss')
     .pipe(sass({errLogToConsole: true}))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('src/css'));
+    .pipe(gulp.dest('src/css'))
+    .pipe(reload({stream:true}));
 });
 
-// Livereload
-gulp.task('listen', function(next) {
-    server.listen(35729, function(err) {
-        if (err) return console.log;
-        next();
+// BrowserSync
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./output",
+            index: "test-template.html"
+        },
+        open: "external",
+        logPrefix: "Gulp Email Creator"
     });
 });
 
@@ -35,16 +39,16 @@ gulp.task('build', function() {
                 removeStyleTags: true,
                 removeLinkTags: true
         }))
-        .pipe(gulp.dest('output/'))
-        .pipe(livereload(server));
+        .pipe(gulp.dest('./output'))
+        .pipe(reload({stream:true}));
 });
- 
+
 // Watch Files For Changes
 gulp.task('watch', function() {
     gulp.watch('src/scss/*.scss', ['sass']);
     gulp.watch('src/html/*.html', ['build']);
     gulp.watch('src/css/*.css', ['build']);
 });
- 
+
 // Default Task
-gulp.task('default', ['sass', 'listen', 'build', 'watch']);
+gulp.task('default', ['sass', 'browser-sync', 'build', 'watch']);
